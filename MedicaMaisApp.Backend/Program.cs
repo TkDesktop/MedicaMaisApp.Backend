80523c8dcb88c81ba3d57f43e502963fd664167e
TkDesktop <tharyckhenrique15@gmail.com>
2026-09-24 20:31:08 -0300

    resend

--- MedicaMaisApp.Backend/Program.cs
+++ MedicaMaisApp.Backend/Program.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Identity.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
-
+using Resend;
 
 var builder = WebApplication.CreateBuilder(args);
 
