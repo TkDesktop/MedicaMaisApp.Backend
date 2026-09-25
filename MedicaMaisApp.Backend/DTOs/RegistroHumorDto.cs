420fe048a59163d119a86c8c34efadd6ecc4c2d3
TkDesktop <tharyckhenrique15@gmail.com>
2026-09-25 20:55:20 -0300

    a

--- MedicaMaisApp.Backend/DTOs/RegistroHumorDto.cs
+++ MedicaMaisApp.Backend/DTOs/RegistroHumorDto.cs
@@ -1,10 +1,21 @@
+using System.ComponentModel.DataAnnotations;
 using MedicaMaisApp.Backend.Modelos;
 
 namespace MedicaMaisApp.Backend.DTOs
 {
-    public class RegistroHumorDto
+    public class RegistroHumorDto : IValidatableObject
     {
         public DateTime? DataHora { get; set; }
         public TipoHumor Humor { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (!Enum.IsDefined(typeof(TipoHumor), Humor))
+            {
+                yield return new ValidationResult(
+                    "O tipo de humor informado é inválido.",
+                    new[] { nameof(Humor) });
+            }
+        }
     }
 }
