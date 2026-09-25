3ecbfef807d5745b45fca000b126f07bc99c3331
TkDesktop <tharyckhenrique15@gmail.com>
2026-09-24 21:40:04 -0300

    a

---
+++ MedicaMaisApp.Backend/DTOs/AssinaturaRespostaDto.cs
@@ -0,0 +1,26 @@
+﻿using MedicaMaisApp.Backend.Modelos;
+
+namespace MedicaMaisApp.Backend.DTOs
+{
+    public class AssinaturaRespostaDto
+    {
+        public int Id { get; set; }
+        public int UsuarioId { get; set; }
+        public Plano Plano { get; set; }
+        public decimal Valor { get; set; }
+        public MetodoPagamento MetodoPagamento { get; set; }
+        public StatusPagamento Status { get; set; }
+        public DateTime DataPagamento { get; set; }
+
+        public static AssinaturaRespostaDto DeEntidade(Assinatura assinatura) => new()
+        {
+            Id = assinatura.Id,
+            UsuarioId = assinatura.UsuarioId,
+            Plano = assinatura.Plano,
+            Valor = assinatura.Valor,
+            MetodoPagamento = assinatura.MetodoPagamento,
+            Status = assinatura.Status,
+            DataPagamento = assinatura.DataPagamento
+        };
+    }
+}
