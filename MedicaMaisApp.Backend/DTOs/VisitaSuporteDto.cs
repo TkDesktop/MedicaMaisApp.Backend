9b023754780152dd28e53e3eb2d57de49250a00d
TkDesktop <tharyckhenrique15@gmail.com>
2026-09-25 21:00:26 -0300

    .

--- MedicaMaisApp.Backend/DTOs/VisitaSuporteDto.cs
+++ MedicaMaisApp.Backend/DTOs/VisitaSuporteDto.cs
@@ -1,11 +1,20 @@
 using MedicaMaisApp.Backend.Modelos;
+using System.ComponentModel.DataAnnotations;
 
 namespace MedicaMaisApp.Backend.DTOs
 {
-    public class VisitaSuporteCriacaoDto
+    public class VisitaSuporteCriacaoDto : IValidatableObject
     {
         public DateTime DataHora { get; set; }
         public string? Observacao { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (DataHora <= DateTime.UtcNow)
+            {
+                yield return new ValidationResult("A data e hora da visita devem estar no futuro.",new[] { nameof(DataHora) });
+            }
+        }
     }
 
     public class VisitaReagendamentoDto
